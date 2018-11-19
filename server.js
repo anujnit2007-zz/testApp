e36dc672642adaa272fcc8fd236fcb45302906d2
anujk <anujk@172.29.4.106>
2018-11-19 14:18:47 +0530

    new changes

--- server.js
+++ server.js
@@ -14,31 +14,16 @@ app.use(bodyParser.json())
 
 // define a simple route
 app.get('/', (req, res) => {
-
     res.json({"output": "server is working"});
 });
 app.get('/timezone', (req, res) => {
-    let timezone = req.query.timezone;
-    console.log(timezone);
-    let rawdata = fs.readFileSync('./ne_10m_time_zones.geojson');
+    let rawdata = fs.readFileSync('./world.geojson');
     let jsonObject = JSON.parse(rawdata);
-    console.log("length "+jsonObject.features.length);
-    let index=0;
-    let output=[];
-    for(index=0;index<jsonObject.features.length;index++){
-       //console.log(jsonObject.features[index].properties.zone);
-    if(jsonObject.features[index].properties.zone==timezone)
-    {
-        output.push(jsonObject.features[index].geometry.coordinates[0]);
-    }
-}
-res.header('Access-Control-Allow-Origin', '*');
-    res.json({"output": output});
+//    console.log("length "+jsonObject.features.length);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.json({"output": jsonObject.features});
 });
-
-
 // listen for requests
-
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
 console.log("Listening on Port 3000");
